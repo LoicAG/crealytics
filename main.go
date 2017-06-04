9e38024e87dee74236d5147303122885802287b6
Loic Antoine-Gombeaud <loic.antoinegombeaud@gmail.com>
2017-06-04 13:24:25 +0200

    Implement CreateInstance

--- main.go
+++ main.go
@@ -1,22 +1,35 @@
 package main
 
 import (
-    "net/http"
+    "fmt"
+    "io/ioutil"
     "github.com/gin-gonic/gin"
+    "golang.org/x/oauth2/google"
     "google.golang.org/api/compute/v1"
 )
 
 func main() {
     r := gin.Default()
 
-    computeService, err := compute.New(&http.Client{}) //TODO configure the http.Client
+    configBytes, err := ioutil.ReadFile("./credentials.json")
+    if err != nil {
+        panic(err)
+    }
 
+    jwtConfig, err := google.JWTConfigFromJSON(configBytes, "https://www.googleapis.com/auth/compute")
+    if err != nil {
+        panic(err)
+    }
+
+    httpClient := jwtConfig.Client(nil)
+
+    service, err := compute.New(httpClient) //TODO configure the http.Client
     if err != nil {
         panic(err)
     }
 
     r.GET("/healthcheck", HealthCheck)
-    r.POST("/v1/instances/create", CreateInstance(computeService))
+    r.POST("/v1/instances/create", CreateInstance(service))
 
     r.Run(":8080")
 }
@@ -25,10 +38,42 @@ func HealthCheck(c *gin.Context) {
     c.String(200, "")
 }
 
-func CreateInstance(computeService *compute.Service) gin.HandlerFunc {
+func CreateInstance(service *compute.Service) gin.HandlerFunc {
     fn := func(c *gin.Context) {
-        //TODO call computeService.Whatever
-    }
+        projectId := "crealytics-169710"
+        prefix := "https://www.googleapis.com/compute/v1/projects/" + projectId
+        imageURL := "https://www.googleapis.com/compute/v1/projects/debian-cloud/global/images/debian-7-wheezy-v20140606"
+        zone := "europe-west1-c"
 
+        instance := &compute.Instance{
+	    Name: "dummy",
+            MachineType: prefix + "/zones/" + zone + "/machineTypes/f1-micro",
+            NetworkInterfaces: []*compute.NetworkInterface{
+                &compute.NetworkInterface{
+                    AccessConfigs: []*compute.AccessConfig{
+                        &compute.AccessConfig{
+                            Type: "ONE_TO_ONE_NAT",
+                            Name: "External NAT",
+                        },
+                    },
+                    Network: prefix + "/global/networks/default",
+                },
+            },
+            Disks: []*compute.AttachedDisk{
+                {
+                    AutoDelete: true,
+                    Boot:       true,
+                    Type:       "PERSISTENT",
+                    InitializeParams: &compute.AttachedDiskInitializeParams{
+                        DiskName:    "my-root-pd",
+                        SourceImage: imageURL,
+                    },
+                },
+            },
+	}
+
+        op, err := service.Instances.Insert(projectId, zone, instance).Do()
+        fmt.Printf("Got compute.Operation, err: %#v, %v", op, err)
+    }
     return gin.HandlerFunc(fn)
 }
